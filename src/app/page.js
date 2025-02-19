4a39cfe28624a1d4209c8f635ea5ca04d2be4ba7
Oyunbat <oyunbat9958@gmail.com>
2025-02-18 18:57:31 -0600

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,54 +1,52 @@
 "use client";
 import Header from "@/components/Header";
-import UserInformationStep from "@/components/UserInformationStep"
-import ContinueButton from "@/components/ContinueButton"
+import UserInformationStep from "@/components/UserInformationStep";
+
 import ContactStep from "@/components/ContactStep";
 import { useState } from "react";
 
 export default function Home() {
-  const [currentStep , setcurrentStep] = useState(0)
-  const [formValues , setFormValues] = useState({
+  const [currentStep, setcurrentStep] = useState(0);
+  const [formValues, setFormValues] = useState({
     firstName: "",
     lastName: "",
     userName: "",
     email: "",
     phoneNumber: "",
     password: "",
-  })
+  });
 
-  const [formErrors , setFormErrors] = useState({
+  const [formErrors, setFormErrors] = useState({
     firstName: "",
     lastName: "",
     userName: "",
     email: "",
     phoneNumber: "",
     password: "",
-  })
-  const prevStep = () =>{
-    setcurrentStep((prev) => prev-1)
-  }
-  const nextStep = () =>{
-    if(formValues.firstName == ""){
-     
-      setFormErrors((prev) => ({...prev, firstName: "Нэрээ оруулна уу"}))
-      return;
-    }else{
-      setcurrentStep((prev) => prev+1)
-    }
-  }
+  });
+
   return (
     <div className="bg-slate-700 h-screen w-screen">
       <div className="flex flex-col justify-center items-center h-screen gap-5">
         <div className="w-[380px] bg-white h-[555px] rounded-xl  ">
           <div className="p-[32px] flex flex-col h-full justify-around gap-2 relative">
-            <Header/>
-            <form>
-              {currentStep === 0 && <UserInformationStep formErrors={formErrors} formValues ={formValues} setFormValues={setFormValues}></UserInformationStep>}
-              {currentStep === 1 && <ContactStep  formValues ={formValues} setFormValues={setFormValues} ></ContactStep>}
-            </form>
-            <ContinueButton currentStep = {currentStep+1} nextStep ={nextStep} prevStep={prevStep}/>
+            <Header />
+            {currentStep === 0 && (
+              <UserInformationStep
+                formErrors={formErrors}
+                formValues={formValues}
+                setFormValues={setFormValues}
+                setcurrentStep={setcurrentStep}
+                setFormErrors={setFormErrors}
+              ></UserInformationStep>
+            )}
+            {currentStep === 1 && (
+              <ContactStep
+                formValues={formValues}
+                setFormValues={setFormValues}
+              ></ContactStep>
+            )}
           </div>
-          
         </div>
       </div>
     </div>
--- src/components/UserInformationStep.jsx
+++ src/components/UserInformationStep.jsx
@@ -1,50 +1,79 @@
+import ContinueButton from "./ContinueButton";
 const UserInformationStep = (props) => {
-    const onFirstNameChange = (event)=>{
-      props.setFormValues((prev) => ({...prev , firstName: event.target.value}));
-    }
-    const onLastNameChange = (event)=>{
-      props.setFormValues((prev) => ({...prev , lastName: event.target.value}));
-    }
-    const onUserNameChange = (event)=>{
-      props.setFormValues((prev) => ({...prev , userName: event.target.value}));
-    }
-    return (
-
-        <div className=" flex flex-col gap-2 mt-[106px] mb-[100px]">
-          <div className="flex flex-col">
-            <label className="text-[#334155 font-[600] tracking-normal text-[12px] ">
-              First name <span className="text-red-600">*</span>
-            </label>
-            <input
-              className={`${props.formErrors.firstName == "" ? "border-grey-400 border-[1px]   focus:border-blue-600 focus:ring-1 focus:outline-none py-[6px] rounded-md placeholder:text-[14px] placeholder:font-[600] placeholder:pl-2" : "border-red-600 border-[1px]  focus:border-blue-600 focus:ring-1 focus:outline-none py-[6px] rounded-md placeholder:text-[14px] placeholder:font-[600] placeholder:pl-2"}`}
-              placeholder="Your first name" onChange={onFirstNameChange}
-            ></input>
-            {
-              props.formErrors.firstName && <p className="text-red-600 text-[10px]">{props.formErrors.firstName}</p>
-            }
-          </div>
-          <div className="flex flex-col">
-            <label className="text-[#334155 font-[600] tracking-normal text-[12px] pb-[4px]">
-              Last name <span className="text-red-600">*</span>
-            </label>
-            <input
-              className="border-gray-400 border-[1px]  focus:border-blue-600 focus:ring-1 focus:outline-none py-[6px] rounded-md placeholder:text-[14px] placeholder:font-[600] placeholder:pl-2"
-              placeholder="Your last name" onChange={onLastNameChange}
-            ></input>
-          </div>
-          <div className="flex flex-col">
-            <label className="text-[#334155 font-[600] tracking-normal text-[12px] pb-[4px]">
-              Username <span className="text-red-600">*</span>
-            </label>
-            <input
-              className="border-gray-400 border-[1px]  focus:border-blue-600 focus:ring-1 focus:outline-none py-[6px] rounded-md placeholder:text-[14px] placeholder:font-[600] placeholder:pl-2"
-              placeholder="Your username " onChange={onUserNameChange}
-            ></input>
-          </div>
-        </div>
-   
-    );
+  const onChange = (event) => {
+    const { setFormValues } = props;
+    setFormValues((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
   };
+  const nextStep = () => {
+    props.setcurrentStep((prev) => prev + 1);
+  };
+  const handleNext = (event) => {
+    event.preventDefault();
+    if (!props.formValues.firstName) {
+      props.setFormErrors((prev) => ({
+        ...prev,
+        firstName: "Нэрээ оруулна уу",
+      }));
+      return;
+    }
+    if (!formValues.lastName) {
+      setFormErrors((prev) => ({ ...prev, lastName: "Нэрээ оруулна уу" }));
+      return;
+    }
+    nextStep();
+  };
+  return (
+    <form onSubmit={handleNext}>
+      <div className=" flex flex-col gap-2 mt-[106px] mb-[100px]">
+        <div className="flex flex-col">
+          <label className="text-[#334155 font-[600] tracking-normal text-[12px] ">
+            First name <span className="text-red-600">*</span>
+          </label>
+          <input
+            className={`${
+              props.formErrors.firstName == ""
+                ? "border-grey-400 border-[1px]   focus:border-blue-600 focus:ring-1 focus:outline-none py-[6px] rounded-md placeholder:text-[14px] placeholder:font-[600] placeholder:pl-2"
+                : "border-red-600 border-[1px]  focus:border-blue-600 focus:ring-1 focus:outline-none py-[6px] rounded-md placeholder:text-[14px] placeholder:font-[600] placeholder:pl-2"
+            }`}
+            placeholder="Your first name"
+            name="firstName"
+            onChange={onChange}
+          ></input>
+          {props.formErrors.firstName && (
+            <p className="text-red-600 text-[10px]">
+              {props.formErrors.firstName}
+            </p>
+          )}
+        </div>
+        <div className="flex flex-col">
+          <label className="text-[#334155 font-[600] tracking-normal text-[12px] pb-[4px]">
+            Last name <span className="text-red-600">*</span>
+          </label>
+          <input
+            className="border-gray-400 border-[1px]  focus:border-blue-600 focus:ring-1 focus:outline-none py-[6px] rounded-md placeholder:text-[14px] placeholder:font-[600] placeholder:pl-2"
+            placeholder="Your last name"
+            name="lastName"
+            onChange={onChange}
+          ></input>
+        </div>
+        <div className="flex flex-col">
+          <label className="text-[#334155 font-[600] tracking-normal text-[12px] pb-[4px]">
+            Username <span className="text-red-600">*</span>
+          </label>
+          <input
+            className="border-gray-400 border-[1px]  focus:border-blue-600 focus:ring-1 focus:outline-none py-[6px] rounded-md placeholder:text-[14px] placeholder:font-[600] placeholder:pl-2"
+            placeholder="Your username "
+            name="userName"
+            onChange={onChange}
+          ></input>
+        </div>
+        <ContinueButton currentStep={props.currentStep + 1} />
+      </div>
+    </form>
+  );
+};
 
-
-export default UserInformationStep
+export default UserInformationStep;
